591a20111cb14994d04798935734a8e53b3549dd
Markus Goetz <markus.goetz@kit.edu>
2021-02-23 15:33:13 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import codecs
+
+
+with codecs.open("README.md", "r", "utf-8") as handle:
+    long_description = handle.read()
+
+# __version__ = None  # appeases flake, assignment in exec() below
+# with open("./heat/core/version.py") as handle:
+#    exec(handle.read())
+
+setup(
+    name="hyperspectral denoising",
+    packages=find_packages(exclude=(,)),
+    data_files=["README.md", "LICENSE"],
+    version=__version__,
+    description="Hyperspectral Denoising algorithm toolbox",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Helmholtz AI Local Energy",
+    author_email="markus.goetz@kit.edu",
+    url="https://github.com/Helmholtz-AI-Energy/HyperspectralDenoising",
+    keywords=["hyperspectral", "denoising", "remote sensing", "gpu"],
+    python_requires="~=3.8",
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: BSD-3 License",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering",
+    ],
+    install_requires=["numpy>=1.15.0", "torch>=1.7.0"]
+)
