7a8e0f6c6938695cb3cc52b90359612c4f85d89f
coquelin77 <daniel.coquelin@gmail.com>
2021-05-26 11:27:28 +0200

    made setup more explicit

--- setup.py
+++ setup.py
@@ -6,11 +6,44 @@
     PyScaffold helps you to put up the scaffold of your new Python project.
     Learn more under: https://pyscaffold.org/
 """
-from setuptools import setup
+import setuptools
 
 if __name__ == "__main__":
+    with open("README.md", "r", encoding="utf-8") as fh:
+        long_description = fh.read()
+
     try:
-        setup(use_scm_version={"version_scheme": "no-guess-dev"})
+        # setup(use_scm_version={"version_scheme": "no-guess-dev"})
+        setuptools.setup(
+            name="hyde",
+            description="Hyperspectral Denoising algorithm toolbox in Python for GPUs",
+            version="0.1.0",
+            author="Helmholtz-AI-Energy",
+            author_email="consultant-helmholtz.ai@kit.edu",
+            license="BSD-3-Clause",
+            long_description=long_description,
+            url="https://github.com/Helmholtz-AI-Energy/HyDe",
+            long_description_content_type="text/markdown",
+            project_urls={
+                "Source": " https://github.com/Helmholtz-AI-Energy/HyDe",
+                "Changelog": "https://github.com/Helmholtz-AI-Energy/HyDe/blob/main/CHANGELOG.md",
+            },
+            classifiers=[
+                "Development Status :: 4 - Beta",
+                "License :: OSI Approved :: BSD License",
+                "Programming Language :: Python",
+                "Programming Language :: Python :: 3",
+                "Programming Language :: Python :: 3.9",
+                "Programming Language :: Python :: 3.8",
+                "Programming Language :: Python :: 3.7",
+                "Programming Language :: Python :: 3.6",
+                "Intended Audience :: Science/Research",
+                "Topic :: Scientific/Engineering",
+            ],
+            package_dir={"": "src"},
+            packages=setuptools.find_packages(where="src"),
+            python_requires=">=3.6",
+        )
     except:  # noqa
         print(
             "\n\nAn error occurred while building the project, "
