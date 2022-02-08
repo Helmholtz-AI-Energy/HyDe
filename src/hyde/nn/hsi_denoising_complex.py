cfca62a90c9fa0c9201b19a605c9d6bf7c12c5eb
coquelin77 <daniel.coquelin@gmail.com>
2022-02-08 10:24:05 +0100

    package refactor, new files

---
+++ src/hyde/nn/hsi_denoising_complex.py
@@ -0,0 +1,200 @@
+import argparse
+import os
+from os.path import join
+
+import numpy as np
+import torch
+import torch.backends.cudnn as cudnn
+import torch.nn as nn
+import torch.optim as optim
+
+# import torchvision
+import torchvision.transforms as transforms
+from kornia.losses import SSIMLoss
+from torch.utils.data import DataLoader
+
+from hyde.lowlevel import logging
+from hyde.nn import comm
+from hyde.nn import dataset_utils as ds_utils
+from hyde.nn import helper, training_utils
+from hyde.nn.general_nn_utils import (
+    AddNoiseDeadline,
+    AddNoiseImpulse,
+    AddNoiseNonIIDdB,
+    AddNoiseStripe,
+    MultipleWeightedLosses,
+)
+from hyde.nn.parsers import train_argparse
+from hyde.nn.qrnn3d import lmdb_dataset, models
+
+logger = logging.get_logger()
+
+
+def main():
+    """Training settings"""
+    parser = argparse.ArgumentParser(
+        description="QRNN3D Hyperspectral Image Denoising (Gaussian Noise)"
+    )
+    # cla == command line arguments
+    cla = train_argparse(parser)
+    logger.debug(cla)
+
+    # self.prefix = cla.prefix
+    # parser.add_argument("--prefix", "-p", type=str, default="denoise", help="prefix")
+    prefix = cla.prefix
+
+    # Engine.setup(self):
+    basedir = join("checkpoints", cla.arch)
+    if not os.path.exists(basedir):
+        os.makedirs(basedir)
+
+    if torch.cuda.is_available:
+        cuda = True
+    device = "cuda" if cuda else "cpu"
+    # todo: if there are multiple devices, then launch torch distributed
+    #       get from MLPerf
+    logger.debug("Cuda Acess: %d" % cuda)
+    if cuda and not torch.cuda.is_available():
+        raise Exception("No GPU found, please run without --cuda")
+
+    torch.manual_seed(cla.seed)
+    if cuda:
+        torch.cuda.manual_seed(cla.seed)
+
+    """Model"""
+    logger.info(f"=> creating model: {cla.arch}")
+    net = models.__dict__[cla.arch]()
+    # initialize parameters
+    # init params will set the model params with a random distribution
+    helper.init_params(net, init_type=cla.init)  # disable for default initialization
+
+    if torch.cuda.device_count() > 1 and cuda:
+        group = comm.init(method="nccl-mpi")
+        net = nn.parallel.DistributedDataParallel(net, device_ids=cla.gpu_ids)
+        net = nn.SyncBatchNorm.convert_sync_batchnorm(net, group)
+
+    if cla.loss == "l2":
+        criterion = nn.MSELoss()
+    elif cla.loss == "l1":
+        criterion = nn.L1Loss()
+    elif cla.loss == "smooth_l1":
+        criterion = nn.SmoothL1Loss()
+    elif cla.loss == "ssim":
+        criterion = SSIMLoss(window_size=11, max_val=1)
+    elif cla.loss == "l2_ssim":
+        criterion = MultipleWeightedLosses(
+            [nn.MSELoss(), SSIMLoss(window_size=11, max_val=1)], weight=[1, 2.5e-3]
+        )
+
+    logger.info(criterion)
+
+    if cuda:
+        net.to(device)
+        criterion = criterion.to(device)
+
+    writer = None
+    if cla.tensorboard:
+        writer = helper.get_summary_writer(os.path.join(cla.basedir, "logs"), cla.cla.prefix)
+
+    """Optimization Setup"""
+    optimizer = optim.Adam(net.parameters(), lr=cla.lr, weight_decay=cla.wd, amsgrad=False)
+
+    # """Resume previous model"""
+    if cla.resume:
+        # Load checkpoint.
+        torch.load(cla.resume_path, not cla.no_ropt)
+    else:
+        logger.info("==> Building model..")
+        logger.info(net)
+
+    cudnn.benchmark = True
+
+    tr_ds = lmdb_dataset.LMDBDataset(cla.dataroot, repeat=64)
+
+    train_transform = transforms.Compose(
+        [
+            AddNoiseNonIIDdB(),
+            transforms.RandomApply(
+                [AddNoiseImpulse(), AddNoiseStripe(), AddNoiseDeadline()], p=0.25
+            ),
+        ]
+    )
+    common_transforms = (transforms.RandomCrop((32, 32)),)
+
+    set_icvl_64_31_TL_1 = ds_utils.ICVLDataset(
+        tr_ds,
+        transform=train_transform,
+        common_transforms=common_transforms,
+    )
+    # worker_init_fn is in dataset -> just getting the seed
+    icvl_64_31_TL = DataLoader(
+        set_icvl_64_31_TL_1,
+        batch_size=cla.batch_size,
+        shuffle=True,
+        num_workers=cla.workers,
+        pin_memory=torch.cuda.is_available(),
+        worker_init_fn=None,
+        persistent_workers=True,
+    )
+    # -------------- validation loader -------------------------
+
+    """Test-Dev"""
+    basefolder = cla.val_datadir
+
+    val_dataset = ds_utils.ICVLDataset(
+        os.path.join(basefolder, "test"), transform=AddNoiseNonIIDdB(), val=True  # complex noise
+    )
+
+    val_loaders = [
+        DataLoader(
+            val_dataset,
+            batch_size=32,
+            shuffle=False,
+            num_workers=cla.workers,
+            pin_memory=torch.cuda.is_available(),
+        )
+    ]
+
+    val_dataset = ds_utils.ICVLDataset(
+        os.path.join(basefolder, "test"), transform=train_transform, val=True  # complex/mixed noise
+    )
+
+    val_loaders.append(
+        DataLoader(
+            val_dataset,
+            batch_size=32,
+            shuffle=False,
+            num_workers=cla.workers,
+            pin_memory=torch.cuda.is_available(),
+        )
+    )
+
+    base_lr = cla.lr
+    helper.adjust_learning_rate(optimizer, cla.lr)
+    epoch_per_save = cla.save_freq
+    max_epochs = 100
+    for epoch in range(max_epochs):
+        s = torch.random.seed()
+        torch.cuda.manual_seed(s)
+        np.random.seed(s)
+
+        if epoch == 85:
+            helper.adjust_learning_rate(optimizer, base_lr * 0.1)
+        elif epoch == 95:
+            helper.adjust_learning_rate(optimizer, base_lr * 0.01)
+
+        training_utils.train(icvl_64_31_TL, net, cla, epoch, optimizer, criterion, writer=writer)
+        training_utils.validate(
+            val_loaders[0], "icvl-validate-noniid", net, cla, epoch, criterion, writer=writer
+        )
+        training_utils.validate(
+            val_loaders[1], "icvl-validate-mixture", net, cla, epoch, criterion, writer=writer
+        )
+
+        helper.display_learning_rate(optimizer)
+        if (epoch % epoch_per_save == 0 and epoch > 0) or epoch == max_epochs - 1:
+            logger.info("Saving current network...")
+            model_latest_path = os.path.join(basedir, prefix, "model_latest.pth")
+            training_utils.save_checkpoint(
+                cla, epoch, net, optimizer, model_out_path=model_latest_path
+            )
---
+++ src/hyde/nn/models/qrnn/redc3d.py
@@ -0,0 +1,55 @@
+import torch.nn as nn
+
+from . import combinations
+
+
+class REDC3D(nn.Module):
+    """Residual Encoder-Decoder Convolution 3D
+    Args:
+        downsample: downsample times, None denotes no downsample"""
+
+    def __init__(self, in_channels, channels, num_half_layer, downsample=None):
+        super(REDC3D, self).__init__()
+        # Encoder
+        assert downsample is None or 0 < downsample <= num_half_layer
+        interval = num_half_layer // downsample if downsample else num_half_layer + 1
+
+        self.feature_extractor = combinations.BNReLUConv3d(in_channels, channels)
+        self.encoder = nn.ModuleList()
+        for i in range(1, num_half_layer + 1):
+            if i % interval:
+                encoder_layer = combinations.BNReLUConv3d(channels, channels)
+            else:
+                encoder_layer = combinations.BNReLUConv3d(
+                    channels, 2 * channels, k=3, s=(1, 2, 2), p=1
+                )
+                channels *= 2
+            self.encoder.append(encoder_layer)
+        # Decoder
+        self.decoder = nn.ModuleList()
+        for i in range(1, num_half_layer + 1):
+            if i % interval:
+                decoder_layer = combinations.BNReLUDeConv3d(channels, channels)
+            else:
+                decoder_layer = combinations.BNReLUUpsampleConv3d(channels, channels // 2)
+                channels //= 2
+            self.decoder.append(decoder_layer)
+        self.reconstructor = combinations.BNReLUDeConv3d(channels, in_channels)
+
+    def forward(self, x):
+        num_half_layer = len(self.encoder)
+        xs = [x]
+        out = self.feature_extractor(xs[0])
+        xs.append(out)
+        for i in range(num_half_layer - 1):
+            out = self.encoder[i](out)
+            xs.append(out)
+        out = self.encoder[-1](out)
+        out = self.decoder[0](out)
+        for i in range(1, num_half_layer):
+            out = out + xs.pop()
+            out = self.decoder[i](out)
+        out = out + xs.pop()
+        out = self.reconstructor(out)
+        out = out + xs.pop()
+        return out
